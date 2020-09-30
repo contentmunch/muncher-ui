6e907f38b2408701635ab0caa1a1dacb3827fe62
asikpradhan@gmail.com <tsb-3DS-ngG-J9K>
2020-09-30 18:11:05 +0545

    feat: add ui elements

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,27 @@
 import Backdrop from "./backdrop/Backdrop";
 import Badge from "./badge/Badge";
 import Button from "./button/Button";
+import Checkbox from "./checkbox/Checkbox";
 import DropdownButton from "./button/DropdownButton";
+import Icon from "./icon/Icon";
 import TextInput from "./input/TextInput";
 import TextInputDropdown from "./input/TextInputDropdown";
-import Icon from "./icon/Icon";
+import Modal from "./modal/Modal";
+import Pill from "./pill/Pill";
+import RangeSlider from "./slider/RangeSlider";
+import Spinner from "./spinner/Spinner";
+import Star from "./star/Star";
 
 export {Backdrop};
 export {Badge};
 export {Button};
+export {Checkbox}
 export {DropdownButton};
 export {TextInput};
 export {TextInputDropdown};
 export {Icon};
+export {Modal};
+export {Pill};
+export {RangeSlider};
+export {Spinner};
+export {Star};
