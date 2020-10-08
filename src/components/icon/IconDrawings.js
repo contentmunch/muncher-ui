062d1c18151b15190109f211b123adc6a75b55d9
asikpradhan@gmail.com <tsb-3DS-ngG-J9K>
2020-10-08 08:10:35 +0545

    feat: color changes for toggle button

--- src/components/icon/IconDrawings.js
+++ src/components/icon/IconDrawings.js
@@ -342,12 +342,12 @@ export const iconDrawings = {
         'toggle-left':
             <Fragment>
                 <rect x="1" y="5" width="22" height="14" rx="7" ry="7"/>
-                <circle cx="8" cy="12" r="3"/>
+                <circle cx="8" cy="12" r="3" color="grey"/>
             </Fragment>,
         'toggle-right':
             <Fragment>
                 <rect x="1" y="5" width="22" height="14" rx="7" ry="7"/>
-                <circle cx="16" cy="12" r="3"/>
+                <circle cx="16" cy="12" r="3" color="green"/>
             </Fragment>,
         'type':
             <Fragment>
