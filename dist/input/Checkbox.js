e2a7035bc08be3e9d024f77b2855771955c6b044
asikpradhan <asikpradhan@users.noreply.github.com>
2020-10-15 15:53:03 +0000

    update build

--- dist/input/Checkbox.js
+++ dist/input/Checkbox.js
@@ -1,11 +1,13 @@
 "use strict";
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
 
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
 
 require("./assets/Input.scss");
 
@@ -15,7 +17,23 @@ var _Label = _interopRequireDefault(require("./Label"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
 
@@ -23,12 +41,16 @@ function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) r
 
 var Checkbox = function Checkbox(_ref) {
   var name = _ref.name,
-      value = _ref.value,
       required = _ref.required,
-      onChange = _ref.onChange,
+      _onChange = _ref.onChange,
       checked = _ref.checked,
       label = _ref.label,
-      props = _objectWithoutProperties(_ref, ["name", "value", "required", "onChange", "checked", "label"]);
+      props = _objectWithoutProperties(_ref, ["name", "required", "onChange", "checked", "label"]);
+
+  var _useState = (0, _react.useState)(checked ? checked : false),
+      _useState2 = _slicedToArray(_useState, 2),
+      checkboxChecked = _useState2[0],
+      setCheckboxChecked = _useState2[1];
 
   return /*#__PURE__*/_react.default.createElement("div", {
     className: "muncher-input--div"
@@ -37,9 +59,12 @@ var Checkbox = function Checkbox(_ref) {
   }, /*#__PURE__*/_react.default.createElement("input", _extends({
     type: "checkbox",
     name: name,
-    value: value,
-    onChange: onChange,
-    checked: checked,
+    onChange: function onChange(event) {
+      setCheckboxChecked(event.target.checked);
+
+      _onChange(event);
+    },
+    checked: checkboxChecked,
     required: required
   }, props)), label ? /*#__PURE__*/_react.default.createElement(_Label.default, {
     label: label,
@@ -52,7 +77,6 @@ Checkbox.propTypes = {
   name: _propTypes.default.string,
   onChange: _propTypes.default.func,
   required: _propTypes.default.bool,
-  value: _propTypes.default.any,
   checked: _propTypes.default.bool,
   label: _propTypes.default.string
 };
