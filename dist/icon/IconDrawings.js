8d96ddc637db6ce2e461bc0e0807a6f7a165d85e
asikpradhan <asikpradhan@users.noreply.github.com>
2020-10-16 10:29:59 +0000

    update build

--- dist/icon/IconDrawings.js
+++ dist/icon/IconDrawings.js
@@ -664,6 +664,11 @@ var iconDrawings = {
   }), /*#__PURE__*/_react.default.createElement("path", {
     d: "M7 11V7a5 5 0 0 1 9.9-1"
   })),
+  'inbox': /*#__PURE__*/_react.default.createElement(_react.Fragment, null, /*#__PURE__*/_react.default.createElement("polyline", {
+    points: "22 12 16 12 14 15 10 15 8 12 2 12"
+  }), /*#__PURE__*/_react.default.createElement("path", {
+    d: "M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z"
+  })),
   'upload': /*#__PURE__*/_react.default.createElement(_react.Fragment, null, /*#__PURE__*/_react.default.createElement("path", {
     d: "M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"
   }), /*#__PURE__*/_react.default.createElement("polyline", {
