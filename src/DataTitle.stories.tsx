a2ba34387498dd76224e2b3bca2ed59285ac300d
asikpradhan@gmail.com <tsb-3DS-ngG-J9K>
2020-10-26 17:32:19 +0545

    feat: upgrade to react 17

--- src/DataTitle.stories.tsx
+++ src/DataTitle.stories.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import {Meta, Story} from "@storybook/react/types-6-0";
 
 export default {
     title: 'Utils/DataTitle',
-}
+} as Meta;
 
-const Template = (args) => <a href='#home' {...args}>Hover here</a>
+const Template:Story = (args) => <a href='#home' {...args}>Hover here</a>
 export const Default = Template.bind({});
 Default.args = {
     'data-title': 'This will show up on hover'
--- src/button/DropdownButton.tsx
+++ src/button/DropdownButton.tsx
@@ -1,6 +1,6 @@
 import React, {useCallback, useEffect, useRef} from 'react';
 import './assets/DropdownButton.scss';
-import {AllButtonProps, Button, ButtonProps} from "./Button";
+import {Button, ButtonProps} from "./Button";
 
 
 export const DropdownButton: React.FC<DropdownButtonProps> = (
