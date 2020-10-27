f042dbc46a45f9a33a50e7c69f0e7787d2bb4d11
asikpradhan@gmail.com <tsb-3DS-ngG-J9K>
2020-10-27 13:19:05 +0545

    feat: mixins, size, color

--- src/star/Star.tsx
+++ src/star/Star.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import './assets/Star.scss';
 
-export const Star: React.FC<StarProps> = ({rating, color, ...props}) => {
+export const Star: React.FC<StarProps> = ({rating, color, size, ...props}) => {
+
     const style: any = {
         '--r': rating
     }
-
-
+    const className = () => {
+        let starClassName = "muncher-star";
+        if (color) {
+            starClassName += " star-" + color;
+        }
+        if (size) {
+            starClassName += " star-" + size;
+        }
+        return starClassName;
+    };
     return (
-        <span className={color ? "muncher-star star-" + color : "muncher-star"} style={style} {...props} />
+        <span className={className()} style={style} {...props} />
     )
 };
 
 export interface StarProps {
     rating: number;
-    color?: 'green' | 'yellow' | 'orange' | 'red'
+    color?: 'green' | 'yellow' | 'orange' | 'red';
+    size?: 'small' | 'medium' | 'large';
 }
