e85c2af70b6eb712fc79dd3736f28642c83cce94
asikpradhan@gmail.com <tsb-3DS-ngG-J9K>
2020-11-15 08:58:46 +0545

    chore: move node-sass to dependencies

--- src/unit/slider/FeaturedUnitSlider.tsx
+++ src/unit/slider/FeaturedUnitSlider.tsx
@@ -1,9 +1,6 @@
 import React, {useEffect, useState} from "react";
-import {CardSlider} from "../../card/CardSlider";
+import {Card, CardSlider, CardSliderSkeleton, Domain} from "../..";
 import {getFeaturedUnits} from "../service/UnitService";
-import {Card} from "../../card/data/Card";
-import {Domain} from "../../community/data/Domain";
-import {CardSliderSkeleton} from "../../card/CardSliderSkeleton";
 
 export const FeaturedUnitSlider: React.FC<FeaturedUnitSliderProps> = ({domain}) => {
     const [isLoading, setIsLoading] = useState(true);
@@ -15,7 +12,7 @@ export const FeaturedUnitSlider: React.FC<FeaturedUnitSliderProps> = ({domain}) 
         });
     }, [domain]);
     return (
-        isLoading ? <CardSliderSkeleton/> : <CardSlider cards={cards} />
+        isLoading ? <CardSliderSkeleton/> : <CardSlider cards={cards}/>
     );
 }
 
