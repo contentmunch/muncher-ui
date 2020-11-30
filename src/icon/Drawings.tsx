8312d7896cf4220c910ea6c71209ec3aaa4ac77f
asikpradhan@gmail.com <tsb-3DS-ngG-J9K>
2020-11-30 20:59:23 +0545

    feat: copy and clipboard drawings

--- src/icon/Drawings.tsx
+++ src/icon/Drawings.tsx
@@ -478,6 +478,16 @@ export const drawings = {
                 <polyline points="17 21 17 13 7 13 7 21"/>
                 <polyline points="7 3 7 8 15 8"/>
             </Fragment>,
+        'copy':
+            <Fragment>
+                <rect x="9" y="9" width="13" height="13" rx="2" ry="2"/>
+                <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"/>
+            </Fragment>,
+        'clipboard':
+            <Fragment>
+                <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"/>
+                <rect x="8" y="2" width="8" height="4" rx="1" ry="1"/>
+            </Fragment>,
         'bookmark':
             <Fragment>
                 <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"/>
