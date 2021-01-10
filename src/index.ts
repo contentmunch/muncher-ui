5f0ddb103236031e0b014a52c39ce8fda6bd9ed5
asikpradhan@gmail.com <tsb-3DS-ngG-J9K>
2021-01-10 14:17:16 +0545

    chore: add Select to index

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ export {DropdownNavButton} from "./button/DropdownNavButton";
 export {Icon} from "./icon/Icon";
 export {Checkbox} from "./input/Checkbox";
 export {Input} from "./input/Input";
+export {Select} from "./input/Select";
 export {Paper} from "./paper/Paper";
 export {Radio} from "./input/Radio";
 export {TextInputDropdown} from "./input/TextInputDropdown";
