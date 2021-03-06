f0ba3a39ff1d6f390750c25262a02f3820a47585
asikpradhan@gmail.com <tsb-3DS-ngG-J9K>
2021-03-06 09:17:55 +0545

    feat: add danger button

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -70,5 +70,5 @@ export interface ButtonProps extends AllButtonProps {
 
 }
 
-export type Variant = 'primary' | 'secondary' | 'tertiary' | 'transparent';
+export type Variant = 'primary' | 'secondary' | 'tertiary' | 'transparent' | 'danger';
 export type Size = 'small' | 'medium' | 'large';
