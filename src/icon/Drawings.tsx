cb6b763c3b0f9e598c792a91af309ad19b2c2371
asikpradhan@gmail.com <tsb-3DS-ngG-J9K>
2021-08-23 14:05:08 +0545

    feat: add facebook icon, text-area padding

--- src/icon/Drawings.tsx
+++ src/icon/Drawings.tsx
@@ -513,6 +513,10 @@ export const drawings = {
                     d="M10 9.5C10 8.67 9.33 8 8.5 8h-5C2.67 8 2 8.67 2 9.5S2.67 11 3.5 11h5c.83 0 1.5-.67 1.5-1.5z"/>
                 <path d="M8.5 5H10V3.5C10 2.67 9.33 2 8.5 2S7 2.67 7 3.5 7.67 5 8.5 5z"/>
             </Fragment>,
+        'facebook':
+            <Fragment>
+                <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/>
+            </Fragment>,
         'twitter':
             <Fragment>
                 <path
