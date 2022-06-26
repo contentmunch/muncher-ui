124d7b3950832bf25cbdbaa00f766f68e55992ac
asikpradhan <asikpradhan@gmail.com>
2022-06-26 11:46:37 -0400

    feat: unordered list

--- src/list/Ul.tsx
+++ src/list/Ul.tsx
@@ -1,6 +1,5 @@
-import React, {ReactElement} from "react";
+import React from "react";
 import "./assets/Ul.scss";
-import {Li} from "./Li";
 
 export const Ul: React.FC<UlProps> = ({children}) => {
     return (<ul className="muncher-ul">{children}</ul>);
@@ -8,5 +7,5 @@ export const Ul: React.FC<UlProps> = ({children}) => {
 }
 
 export interface UlProps {
-    children: ReactElement<typeof Li> | ReactElement<typeof Li>[];
+
 }
